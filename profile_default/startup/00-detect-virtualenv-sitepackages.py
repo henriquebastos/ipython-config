a3aee43beaed855ec03e2de0460f723389b5f5f6
Henrique Bastos <henrique@bastos.net>
2019-06-23 19:20:26 -0300

    Inject venv sitepackages.

---
+++ profile_default/startup/00-detect-virtualenv-sitepackages.py
@@ -0,0 +1,28 @@
+"""IPython startup script to detect and inject VIRTUAL_ENV's site-packages dirs.
+
+IPython can detect virtualenv's path and injects it's site-packages dirs into sys.path.
+But it can go wrong if IPython's python version differs from VIRTUAL_ENV's.
+
+This module fixes it looking for the actual directories. We use only old stdlib
+resources so it can work with as many Python versions as possible.
+
+References:
+http://stackoverflow.com/a/30650831/443564
+http://stackoverflow.com/questions/122327/how-do-i-find-the-location-of-my-python-site-packages-directory
+https://github.com/ipython/ipython/blob/master/IPython/core/interactiveshell.py#L676
+
+Author: Henrique Bastos <henrique@bastos.net>
+License: BSD
+"""
+import os
+from os.path import join
+import sys
+from glob import glob
+
+
+virtualenv = os.environ.get('VIRTUAL_ENV')
+
+if virtualenv:
+    for path in glob(join(virtualenv, 'lib', 'python*', '*')):
+        if path not in sys.path:
+            sys.path.insert(0, path)
